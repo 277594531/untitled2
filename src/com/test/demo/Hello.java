333602937c396324b5df857d8d5c7bfc78d948ad
secret secret <secret secret>
2016-07-10 18:06:22 +0800

    Initial commit

---
+++ src/com/test/demo/Hello.java
@@ -0,0 +1,10 @@
+package com.test.demo;
+
+/**
+ * Created by gxwhu on 2016/7/9.
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, world.");
+    }
+}
